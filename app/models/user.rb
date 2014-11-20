63f0e85f81490d102b9667dfa2e6dd06ff21880d
Matthew Brelsford <newmatt@Matthews-MacBook-Pro.local>
2014-11-20 17:35:38 -0500

    working on the thing show page. functions for user scores

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,31 @@ class User < ActiveRecord::Base
 
   has_many :things
 
+  def creative_score
+  	score = 0;
+  	things.each do |thing|
+  		score+= thing.get_upvotes(:vote_scope => "creative").count
+  	end
+  	return score
+  end
   
+
+  def inpiring_score
+  	score = 0;
+  	things.each do |thing|
+  		score+= thing.get_upvotes(:vote_scope => "inspiring").count
+  	end
+  	return score
+
+  end
+
+  def beauty_score
+  	score = 0;
+  	things.each do |thing|
+  		score+= thing.get_upvotes(:vote_scope => "beauty").count
+  	end
+  	return score
+
+  end
+
 end
